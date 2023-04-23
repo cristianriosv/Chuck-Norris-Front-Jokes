c139a2707d58351d6808c04aaf0ca33ee5fdf6ba
Cristian Rios <cristianriosv@gmail.com>
2023-04-23 09:32:01 +0200

    feat(views): add joke list view to main page
    - create joke list view inside /views
    - get jokes from jokes hook
    - show in a list the fetched joke

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import JokesList from '@/modules/hooks/views/JokesListView';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello from Chuck Norris Jokes content
+      <JokesList/>
     </main>
   )
 }
