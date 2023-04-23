43efbca152d8a10ec8cd9d86a16d5573d877df2a
Cristian Rios <cristianriosv@gmail.com>
2023-04-23 18:24:39 +0200

    refactor(code): add eslint spaces rules and unify code

--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -2,10 +2,10 @@ import NavBar from '@containers/NavBar';
 import JokesFavoriteView from '@views/JokesFavoriteView';
 
 export default function Favorites() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <NavBar />
-        <JokesFavoriteView/>
-    </main>
-  )
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <NavBar />
+            <JokesFavoriteView/>
+        </main>
+    )
 }
