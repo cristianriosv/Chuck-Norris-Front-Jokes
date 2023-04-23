ffeb596a412e70c6b358d8a05c24053a677a9358
Cristian Rios <cristianriosv@gmail.com>
2023-04-23 18:47:53 +0200

    refactor(code): filter possible empty, undefined or null objects in favorites data base

--- modules/hooks/useJokesDataBase.ts
+++ modules/hooks/useJokesDataBase.ts
@@ -1,7 +1,10 @@
 const LOCAL_STORAGE_FAVORITE_KEY = 'favoritesJokes';
 
 const useJokesDataBase = () => {
-    const findFavoritesAll = (): string[] => localStorage.getItem(LOCAL_STORAGE_FAVORITE_KEY)?.split(',') || [];
+    const findFavoritesAll = (): string[] => {
+        const localStorageData = localStorage.getItem(LOCAL_STORAGE_FAVORITE_KEY)?.split(',') || [];
+        return localStorageData.filter((favorite: string) => !!favorite);
+    }
     const updateFavoritesAll = (jokes: string[]) => localStorage.setItem(LOCAL_STORAGE_FAVORITE_KEY, jokes.toString());
 
     return {
