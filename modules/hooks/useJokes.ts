4bb482849bca8e1949d5d0ade4bbba1db71a7036
Cristian Rios <cristianriosv@gmail.com>
2023-04-23 09:45:13 +0200

    refactor(env): use api constants in jokes hook

--- modules/hooks/useJokes.ts
+++ modules/hooks/useJokes.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { RANDOM_JOKE_URL } from '@constants/api.constants';
 import useRequest from '@hooks/useRequest';
 
 const useJokes = () => {
@@ -8,7 +9,7 @@ const useJokes = () => {
     
     const getRandomJoke = async () => {
         setLoading(true);
-        const randomJoke = await getData('https://api.chucknorris.io/jokes/random');
+        const randomJoke = await getData(RANDOM_JOKE_URL);
         setJokes([...jokes, randomJoke]);
         setLoading(false);
     };
